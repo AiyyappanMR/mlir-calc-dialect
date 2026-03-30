a377a916dd88833eee5d7df1859b16643dca4a81
AiyyappanMR <aiyyappan2004@gmail.com>
2026-03-30 15:14:41 +0530

    add .gitignore and removed build folder

--- build/lib/calc/CMakeFiles/MLIRCalc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../libMLIRCalc.a"
-  "../libMLIRCalc.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/MLIRCalc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/lib/calc/CMakeFiles/install-MLIRCalc-stripped.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-MLIRCalc-stripped"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-MLIRCalc-stripped.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
