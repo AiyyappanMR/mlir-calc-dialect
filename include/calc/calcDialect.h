3562370e886d0cc5140b6889d63598ab3a640390
AiyyappanMR <aiyyappan2004@gmail.com>
2026-05-04 10:21:07 +0530

    Custom Trait implementation

--- include/calc/calcDialect.h
+++ include/calc/calcDialect.h
@@ -2,7 +2,31 @@
 #define CALCDIALECT_H
 
 #include "mlir/IR/Dialect.h"
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/IR/TypeUtilities.h"
 
 #include "calc/calcOpsDialect.h.inc"
+
+namespace calc {
+namespace OpTrait {
+template <typename ConcreteType>
+class OperandsMustbeFloat : public mlir::OpTrait::TraitBase<ConcreteType, OperandsMustbeFloat> {
+public:
+    static mlir::LogicalResult verifyTrait(mlir::Operation *op) {
+            // Loop through all operands 
+            for (mlir::Value operand : op->getOperands()) {
+                // This 'unwraps' tensor<f32> to f32
+                mlir::Type elemType = mlir::getElementTypeOrSelf(operand);
+                
+                if (!mlir::isa<mlir::FloatType>(elemType)) {
+                    return op->emitOpError("requires all operands to have floating-point element types");
+                }
+            }
+            return mlir::success();
+        }
+};
+
+}
+}
 
 #endif
